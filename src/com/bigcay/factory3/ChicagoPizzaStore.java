7ecaee879bd4dec8eaca25478527f1ed466d4fac
Leo <biminglei@gmail.com>
2015-03-03 11:22:06 +0800

    allowing the subclasses to decide

---
+++ src/com/bigcay/factory3/ChicagoPizzaStore.java
@@ -0,0 +1,18 @@
+package com.bigcay.factory3;
+
+public class ChicagoPizzaStore extends PizzaStore {
+
+	@Override
+	protected Pizza createPizza(String type) {
+		Pizza pizza = null;
+
+		if (type.equals("cheese")) {
+			pizza = new ChicagoStyleCheesePizza();
+		} else if (type.equals("clam")) {
+			pizza = new ChicagoStyleClamPizza();
+		}
+
+		return pizza;
+	}
+
+}
---
+++ src/com/bigcay/factory3/NYPizzaStore.java
@@ -0,0 +1,18 @@
+package com.bigcay.factory3;
+
+public class NYPizzaStore extends PizzaStore {
+
+	@Override
+	protected Pizza createPizza(String type) {
+		Pizza pizza = null;
+
+		if (type.equals("cheese")) {
+			pizza = new NYStyleCheesePizza();
+		} else if (type.equals("clam")) {
+			pizza = new NYStyleClamPizza();
+		}
+
+		return pizza;
+	}
+
+}
